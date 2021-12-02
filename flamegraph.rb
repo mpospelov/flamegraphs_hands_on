30bf96f0fd01ef100c478ca62aef40eb60c4fe37
Mikhail Pospelov <mikhail.pospelov@toptal.com>
2021-12-02 11:28:07 +0300

    more complicated

--- flamegraph.rb
+++ flamegraph.rb
@@ -1,7 +1,7 @@
 def flamegraph # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
   require 'stackprof'
   flamegraph_file_path = 'flamegraph.txt'
-  report = StackProf.run(mode: :wall, interval: 1, ignore_gc: true, raw: true) do
+  report = StackProf.run(mode: :cpu, interval: 500, ignore_gc: true, raw: true) do
     yield
   end
 
@@ -12,20 +12,91 @@ def flamegraph # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
   system("stackprof --flamegraph-viewer=#{flamegraph_file_path}")
 end
 
-def a
-  Array.new(100000) { |i| i }.shuffle.sort
-  b
-  c
+def calc(scale)
+  Array.new(scale * 100000) { |i| i }.shuffle.sort
 end
 
-def b
-  Array.new(200000) { |i| i }.shuffle.sort
+class AD
+  def initialize
+    @eh = EH.new
+  end
+
+  def a
+    calc(1)
+    b
+    c
+    @eh.g
+    calc(4)
+  end
+
+  def b
+    calc(2)
+    c
+  end
+
+  def c
+    calc(3)
+    d
+  end
+
+  def d
+    calc(4)
+    @eh.e
+  end
 end
 
-def c
-  Array.new(300000) { |i| i }.shuffle.sort
+class EH
+  def initialize
+    @il = IL.new
+  end
+
+  def e
+    calc(4)
+    f
+  end
+
+  def f
+    calc(4)
+    g
+  end
+
+  def g
+    calc(4)
+    10.times { calc(5) }
+    @il.i
+    h
+  end
+
+  def h
+    calc(5)
+    10.times { calc(1) }
+  end
+end
+
+
+class IL
+  def i
+    calc(9)
+    j
+  end
+
+  def j
+    calc(3)
+    k
+  end
+
+  def k
+    calc(7)
+    15.times { calc(2) }
+    l
+  end
+
+  def l
+    calc(10)
+    5.times { calc(3) }
+  end
 end
 
 flamegraph do
-  a
+  AD.new.a
 end
